b4ad558e946c268c290c06f56296bbb8767c4d6a
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-27 14:20:34 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route, NavLink, Link } from 'react-router-dom';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+import './index.css';
+import App from './components/App/App';
+import registerServiceWorker from './registerServiceWorker';
+
+const router = (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store} >
+    {router}
+  </Provider>, document.getElementById('root'));
+registerServiceWorker();
