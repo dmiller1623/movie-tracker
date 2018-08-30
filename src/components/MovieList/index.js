4ee289ea41da5aaabc5a987b9877f3c49c2061f1
Dennis <dmiller1623@gmail.com>
2018-08-30 16:49:34 -0600

    Refactor Movielist

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
+
 import MovieCard from '../../containers/MovieCard';
 import NavBar from '../../containers/NavBar';
 
 const MovieList = ({ movies, favorites = [] }) => {
   const displayedMovies = movies.map( (movie, index) => {
-    let starColor = favorites.includes(movie.movie_id) ? '#ffd24d' : '#f2f2f2';
+    const starColor = favorites.includes(movie.movie_id) ? '#ffd24d' : '#f2f2f2';
     return <MovieCard {...movie} key={index} starColor={starColor}/>
-  })
+  });
+
   return (
-    <div>
-    <NavBar />
-    <section>
-      {displayedMovies}
-    </section>
-    </div>
+    <main>
+      <NavBar />
+      <section>
+        {displayedMovies}
+      </section>
+    </main>
   )
 }
 
