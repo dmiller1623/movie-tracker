ce2004c8004a5cda9c4bb2b8ac1fe135fc574b0e
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-30 17:08:34 -0600

    Fix code format

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -12,7 +12,7 @@ import { getMovies } from '../../utilities/apiCalls/apiCalls';
 import './App.css';
 
 class App extends Component {
-
+  
   componentDidMount = async () => {
     const movies = await getMovies();
     this.props.populateMovies(movies);
@@ -20,6 +20,7 @@ class App extends Component {
 
   render() {
     const { movies, favorites } = this.props;
+
     return (
       <div className="App">
         <Route exact path = '/' render={() => <MovieList movies={movies} favorites={favorites} /> } />
@@ -36,7 +37,7 @@ const mapStateToProps = state => ({
   favorites: state.favorites
 })
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   populateMovies: (movies) => dispatch(populateMovies(movies))
 });
 
