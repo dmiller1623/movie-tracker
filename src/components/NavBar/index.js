e88ebe104ebabb5f2862e5f2f9df3e42f290f2d9
Laura Whitaker <laura.k.whitaker@gmail.com>
2018-08-30 15:15:12 -0600

    Add route back to / on sign out

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { InitialAccountButtons } from '../../components/InitialAccountButtons';
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom';
+
 import UserAccountBtns from '../UserAccountBtns';
 import { signOutUser } from '../../actions';
 
-
-
 class NavBar extends Component {
+  handleSignOut = () => {
+    this.props.signOutUser();
+    this.props.history.push('/')
+  }
+
   render() {
     let navDisplay;
 
     if (this.props.user.name) {
-      navDisplay =  <UserAccountBtns name={this.props.user.name} signOutUser={this.props.signOutUser}/>;
+      navDisplay =  <UserAccountBtns name={this.props.user.name} handleSignOut={this.handleSignOut}/>;
     } else {
       navDisplay = <InitialAccountButtons />;
     }
@@ -32,4 +37,4 @@ const mapDispatchToProps = dispatch => ({
   signOutUser: () => dispatch(signOutUser())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps, null, {pure: false})(NavBar)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps, null, {pure: false})(NavBar))
--- src/components/UserAccountBtns/index.js
+++ src/components/UserAccountBtns/index.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const UserAccountBtns = ({ name, signOutUser}) => {
+const UserAccountBtns = ({ name, handleSignOut}) => {
   return (
     <div>
       <h3>Hi, {name}!</h3>
       <Link to='/favorites'>
         View Favorites
       </Link>
-      <button onClick={() => {
-        signOutUser()
-      }}>
+      <button onClick={() => handleSignOut()}>
         Sign Out
       </button>
     </div>
