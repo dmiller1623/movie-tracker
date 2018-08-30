2ea92d65f0d902866d1d07a039c6638ab52ca9fa
benjaminpaige <benjamin.paige@gmail.com>
2018-08-30 12:02:18 -0600

    Add post request to add favorites in dbase

--- src/containers/MovieCard/index.js
+++ src/containers/MovieCard/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addFavorite } from '../../actions';
+import { addNewFavorite } from '../../utilities/apiCalls/apiCalls';
 
-const MovieCard = ({title, popularity, poster}) => {
-  const posterUrl = `http://image.tmdb.org/t/p/w185//${poster}`
-  return (
-    <article>
-      <h2>{title}</h2>
-      <img src={posterUrl} alt={title + ' poster'} />
-      <p>{popularity}</p>
-    </article>
-  )
-}
+class MovieCard extends Component {
+  toggleFavorite = async () => {
+    const movie = {...this.props, user_id: this.props.user.id}
+    // if (!this.props.favorites.includes(movie))
+    const newFavorite = await addNewFavorite(movie);
+    // this.props.addFavorite(newFavorite);
+  }
 
-export default MovieCard;
+  render() {
+    const { title, poster_path} = this.props;
+    const posterUrl = `http://image.tmdb.org/t/p/w185//${poster_path}`;
+    return (
+      <article onClick={() => this.toggleFavorite()}>
+        <h2>{title}</h2>
+        <img src={posterUrl} alt={title + ' poster_path'} />
+      </article>
+    );
+  }
+};
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+const mapDispatchToProps = dispatch => ({
+  addFavorite: (movie) => dispatch(addFavorite(movie))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MovieCard);
--- src/utilities/helper/helper.js
+++ src/utilities/helper/helper.js
@@ -2,11 +2,14 @@ const cleanData = (data) => {
   const shortenedResults = data.results.filter( movie => movie.poster_path).slice(0, 11);
   return shortenedResults.map((movie, index) => {
     return {
+      movie_id: index,
       title: movie.title,
-      popularity: movie.popularity,
-      poster: movie.poster_path,
-    }
-  })
-}
+      poster_path: movie.poster_path,
+      release_date: movie.release_date,
+      vote_average: movie.vote_average,
+      overview: movie.overview
+    };
+  });
+};
 
 export default cleanData;
