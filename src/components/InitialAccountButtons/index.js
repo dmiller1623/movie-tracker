1f4bd9d426f4f045ab9eab557e93619d518a22c1
Dennis <dmiller1623@gmail.com>
2018-08-30 16:47:02 -0600

    Refactor intitial buttons

--- src/components/InitialAccountButtons/index.js
+++ src/components/InitialAccountButtons/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const InitialAccountButtons = () => {
-
   return (
     <div>
-        <Link to='/login'>
-          Login
-        </Link>
-        <Link to='/signup'>
-          Sign Up
-        </Link>
+      <Link to='/login'>
+        Login
+      </Link>
+      <Link to='/signup'>
+        Sign Up
+      </Link>
     </div>
   );
 };
