8ae796d53e0c40ee3572a75929c5faa5b32a9034
benjaminpaige <benjamin.paige@gmail.com>
2018-09-01 20:33:54 -0600

    Add testing for Login component

--- src/containers/Login/index.test.js
+++ src/containers/Login/index.test.js
@@ -2,14 +2,110 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Login, mapDispatchToProps } from '../Login';
 import { loginUser, populateFavorites } from '../../actions';
+import { getUser, getFavorites } from '../../utilities/apiCalls/apiCalls';
 
+jest.mock('../../utilities/apiCalls/apiCalls.js');
 
 describe('Login', () => {
   let wrapper;
-  const mockUser = {id: 1, name: "Taylor", password: "password", email: "tman2272@aol.com"}
+  const mockUser = {id: 1, name: "Taylor", password: "password", email: "tman2272@aol.com"};
   describe('Login component', () => {
+    wrapper = shallow(<Login />);
+    const loginUser = jest.fn();
+    const populateFavorites = jest.fn();
 
-  })
+    it('should start with default state values', () => {
+
+      expect(wrapper.state('email')).toEqual('');
+      expect(wrapper.state('password')).toEqual('');
+    });
+
+    it('should change the value of the state object when user inputs information', () => {
+
+      const mockEvent1 = {preventDefault: jest.fn(), target: {name: 'email', value: 'b'}};
+
+      wrapper.find('input').first().simulate('change', mockEvent1);
+
+      expect(wrapper.state('email')).toEqual('b');
+
+      const mockEvent2 = {preventDefault: jest.fn(), target: {name: 'password', value: 'password'}};
+
+      wrapper.find('input').last().simulate('change', mockEvent2);
+
+      expect(wrapper.state('password')).toEqual('password');
+    });
+
+    it('should match the snapshot', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should give an alert if login is submitted without login & password', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+      wrapper.setState({email: 'ben@ben.com', password: ''});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(getUser).not.toHaveBeenCalled();
+
+      wrapper.setState({email: '', password: 'password'});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(getUser).not.toHaveBeenCalled();
+
+      wrapper.setState({email: '', password: ''});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(getUser).not.toHaveBeenCalled();
+
+    });
+    it('should call getUser with the correct params when login is submitted', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+      wrapper = shallow(<Login loginUser={loginUser} populateFavorites={jest.fn()} history={[]}/>);
+
+      wrapper.setState({email: 'ben@ben.com', password: 'password'});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(getUser).toHaveBeenCalledWith('ben@ben.com', 'password');
+    });
+    it('should call loginUser when login is submitted', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+      wrapper = shallow(<Login loginUser={loginUser} populateFavorites={jest.fn()} history={[]}/>);
+
+      wrapper.setState({email: 'ben@ben.com', password: 'password'});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(loginUser).toHaveBeenCalled();
+    });
+    it('should call getFavorites when login is submitted', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+      wrapper = shallow(<Login loginUser={loginUser} populateFavorites={populateFavorites} history={[]}/>);
+
+      wrapper.setState({email: 'ben@ben.com', password: 'password'});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(getFavorites).toHaveBeenCalled();
+    });
+    it('should call populateFavorites when login is submitted', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+      wrapper = shallow(<Login loginUser={loginUser} populateFavorites={populateFavorites} history={[]}/>);
+
+      wrapper.setState({email: 'ben@ben.com', password: 'password'});
+
+      wrapper.find('form').simulate('submit', mockEvent);
+
+      expect(populateFavorites).toHaveBeenCalled();
+    });
+  });
   describe('mapDispatchToProps', () => {
     it('should call dispatch when using a function loginUser', () => {
 
@@ -36,5 +132,5 @@ describe('Login', () => {
       expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
 
     });
-  })
-})
+  });
+});
