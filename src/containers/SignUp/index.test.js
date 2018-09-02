83c18207273ae557ccf4b5ffb27bf118b27161c4
Dennis <dmiller1623@gmail.com>
2018-09-02 09:22:16 -0600

    Add sign up and movie list tests

--- src/containers/SignUp/index.test.js
+++ src/containers/SignUp/index.test.js
@@ -8,8 +8,56 @@ describe('SignUp', () => {
   let wrapper
   describe('SignUp component', () => {
     wrapper = mount(<SignUp />)
-    it('should update state when user enters something in an input', () => {
 
+    it('should match the snapshot', () => {
+      wrapper = shallow(<SignUp />)
+      expect(wrapper).toMatchSnapshot()
+    })
+    it('should update state when user enters something in the name input', () => {
+      const mockEvent = { target: { value: 'Dennis', name: 'name' } }
+      const expectedState = {
+        name: 'Dennis',
+        email: '', 
+        password: ''
+      };
+      wrapper.instance().handleChange(mockEvent)
+      expect(wrapper.state()).toEqual(expectedState);
+    })
+
+    it('should update state when user enters something in the email input', () => {
+      const mockEvent = { target: { value: 'dmill@aol.com', name: 'email' } }
+      const expectedState = {
+        name: 'Dennis',
+        email: 'dmill@aol.com', 
+        password: ''
+      };
+      wrapper.instance().handleChange(mockEvent)
+      expect(wrapper.state()).toEqual(expectedState);
+    })
+
+    it('should update state when user enters something in the paswword input', () => {
+      const mockEvent = { target: { value: 'password', name: 'password' } }
+      const expectedState = {
+        name: 'Dennis',
+        email: 'dmill@aol.com', 
+        password: 'password'
+      };
+      wrapper.instance().handleChange(mockEvent)
+      expect(wrapper.state()).toEqual(expectedState);
+    })
+
+    it('should call submitSignUp on form submit', async () => {
+      window.fetch = jest.fn().mockImplementation(() => 
+      Promise.resolve({
+        ok: true, 
+        json: () => Promise.resolve({ name:'Dennis', email:'dm@aol.com', password:'password' })
+      }));
+      const mockEvent = { preventDefault: jest.fn() }
+      const mockSignUp = jest.fn()
+      const history = []
+      wrapper = shallow(<SignUp signUpUser={mockSignUp} history={history}/>)
+      await wrapper.instance().submitSignUp(mockEvent)
+      expect(mockSignUp).toHaveBeenCalled()
     })
   })
 
